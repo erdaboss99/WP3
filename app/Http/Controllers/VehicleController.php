53655940fd08ff75a7fd21d584127c610e96d1d1
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-06-06 19:25:27 +0200

    Created VehicleController

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVehicleRequest;
+use App\Http\Requests\UpdateVehicleRequest;
+use App\Models\Vehicle;
+
+class VehicleController extends Controller
+{
+
+    public function index()
+    {
+        //TO-DO
+    }
+
+    public function create()
+    {
+        //TO-DO
+    }
+
+    public function store(StoreVehicleRequest $request)
+    {
+        //TO-DO
+    }
+
+    public function show(Vehicle $vehicle)
+    {
+        //TO-DO
+    }
+
+    public function edit(Vehicle $vehicle)
+    {
+        //TO-DO
+    }
+
+    public function update(UpdateVehicleRequest $request, Vehicle $vehicle)
+    {
+        //TO-DO
+
+    }
+
+    public function destroy(Vehicle $vehicle)
+    {
+        //TO-DO
+    }
+}
