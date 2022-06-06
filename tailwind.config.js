24c92a1d72e9f08264fda78840e404424301d69f
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-06-06 18:28:22 +0200

    Fixed an issue
    Tailwind was not loading properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/views/**/**/**/**/*.blade.php',
+        './resources/views/**/**/**/*.blade.php',
+        './resources/views/**/**/*.blade.php',
+        './**/*.blade.php',
     ],
 
     theme: {
